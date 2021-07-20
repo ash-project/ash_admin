b987797ab22766712b6093b4b21a5d42878be72e
Zach Daniel <zach@zachdaniel.dev>
2021-07-20 02:23:14 -0400

    chore: set ash_version properly

--- mix.exs
+++ mix.exs
@@ -86,8 +86,8 @@ defmodule AshAdmin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:ash, "~> 1.46 and >= 1.46.9"},
-      {:ash, path: "../ash", override: true},
+      {:ash, "~> 1.46 and >= 1.46.9"},
+      # {:ash, path: "../ash", override: true},
       # {:ash_phoenix, "~> 0.5 and >= 0.5.1"},
       {:ash_phoenix, path: "../ash_phoenix"},
       {:surface, "~> 0.4.1"},
