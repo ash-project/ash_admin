3e9ed308c4a1f6565e13f9f87a296f159c49da70
Esdras Eduardo <esdrasedu@gmail.com>
2023-07-28 16:49:59 +0800

    Imp: Add CoreCompoment and update assets structure

--- lib/ash_admin/web.ex
+++ lib/ash_admin/web.ex
@@ -1,61 +1,80 @@
 defmodule AshAdmin.Web do
   @moduledoc false
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
-  @doc false
-  def view do
+  def router do
     quote do
-      @moduledoc false
+      use Phoenix.Router, helpers: false
 
-      use Phoenix.Template,
-        namespace: Phoenix.LiveDashboard,
-        root: "lib/ash_admin/templates"
-
-      import AshAdmin.Helpers
-
-      unquote(view_helpers())
+      # Import common connection and controller functions to use in pipelines
+      import Plug.Conn
+      import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
-  @doc false
+  def channel do
+    quote do
+      use Phoenix.Channel
+    end
+  end
+
   def live_view do
     quote do
-      @moduledoc false
-      use Phoenix.LiveView
-      import AshAdmin.Helpers
-      unquote(view_helpers())
+      use Phoenix.LiveView,
+        layout: {AshAdmin.Layouts, :app}
+
+      unquote(html_helpers())
     end
   end
 
-  @doc false
   def live_component do
     quote do
-      @moduledoc false
       use Phoenix.LiveComponent
-      import AshAdmin.Helpers
-      unquote(view_helpers())
+
+      unquote(html_helpers())
     end
   end
 
-  defp view_helpers do
+  def html do
     quote do
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
+      use Phoenix.Component
 
-      case Code.ensure_compiled(Phoenix.Component) do
-        {:module, _} ->
-          import Phoenix.Component
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_csrf_token: 0, view_module: 1, view_template: 1]
 
-        _ ->
-          # Import convenience functions for LiveView rendering
-          import Phoenix.LiveView.Helpers
-      end
+      # Include general helpers for rendering HTML
+      unquote(html_helpers())
+    end
+  end
 
-      import AshAdmin.Helpers
+  defp html_helpers do
+    quote do
+      # HTML escaping functionality
+      import Phoenix.HTML
+      # Core UI components and translation
+      import AshAdmin.CoreComponents
+      import AshAdmin.Gettext
+
+      # Shortcut for generating JS commands
+      alias Phoenix.LiveView.JS
+
+      # Routes generation with the ~p sigil
+      unquote(verified_routes())
+    end
+  end
+
+  def verified_routes do
+    quote do
+      use Phoenix.VerifiedRoutes,
+        router: AshAdmin.Router,
+        statics: AshAdmin.Web.static_paths()
     end
   end
 
   @doc """
-  Convenience helper for using the functions above.
+  When used, dispatch to the appropriate controller/view/etc.
   """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
