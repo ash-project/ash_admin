a253071f91454dfd52ed8c0703e37fa0baad8887
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2023-11-15 14:16:32 +0800

    fix: Fix JS syntax error and compile latest version of `app.js` (#77)
    An upgrade to phoenix_live_view requires recompiling the app.js file.
    
    Closes #72

--- assets/js/app.js
+++ assets/js/app.js
@@ -194,9 +194,9 @@ window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect() >>
-  // expose liveSocket on window for web console debug logs and latency simulation:
-  liveSocket.enableDebug();
-// >> liveSocket.enableLatencySim(1000)
+liveSocket.connect();
+// expose liveSocket on window for web console debug logs and latency simulation:
+liveSocket.enableDebug();
+//  liveSocket.enableLatencySim(1000)
 
 window.liveSocket = liveSocket;
