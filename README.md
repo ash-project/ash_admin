c107815edc5585974d9efba5dfda3ed3eb2fc3ca
Rebecca Le <rebecca@alembic.com.au>
2023-11-21 15:46:22 +0800

    feat: Wrap sensitive attribute rendering in a new `SensitiveAttribute` live component
    This will handle hiding the value by default and having a "click to display" icon
    
    The icon can then be clicked to hide it again

--- README.md
+++ README.md
@@ -116,7 +116,7 @@ Then, you can start the app with: `mix dev`
 
 If you make changes to the resources, you can generate migrations with `mix generate_migrations`
 
-If you make changes to any of the assets (CSS or JavaScript), including updating dependencies that include assets such as LiveView, you will need to recompile the assets with `mix assets.build`.
+If you make changes to any of the assets (CSS or JavaScript), including updating dependencies that include assets such as LiveView, you will need to recompile the assets with `mix assets.deploy`.
 
 ## Contributors
 
---
+++ lib/ash_admin/components/resource/sensitive_attribute.ex
@@ -0,0 +1,34 @@
+defmodule AshAdmin.Components.Resource.SensitiveAttribute do
+  use Phoenix.LiveComponent
+
+  import AshAdmin.CoreComponents
+
+  def mount(socket) do
+    {:ok, assign(socket, viewed: false)}
+  end
+
+  def render(assigns) do
+    assigns = assign(assigns, present?: assigns.value not in [nil, ""])
+
+    ~H"""
+    <div>
+      <span :if={@present? && !@viewed} class="italic">
+        --redacted--
+        <span class="cursor-pointer" phx-click="toggle_sensitive_attribute" phx-target={@myself}>
+          <.icon name="hero-eye-solid" class="w-5 h-5 text-gray-500" />
+        </span>
+      </span>
+      <span :if={@present? && @viewed}>
+        <%= render_slot(@inner_block) %>
+        <span class="cursor-pointer" phx-click="toggle_sensitive_attribute" phx-target={@myself}>
+          <.icon name="hero-eye-slash-solid" class="w-5 h-5 text-gray-500" />
+        </span>
+      </span>
+    </div>
+    """
+  end
+
+  def handle_event("toggle_sensitive_attribute", _params, socket) do
+    {:noreply, assign(socket, viewed: !socket.assigns.viewed)}
+  end
+end
