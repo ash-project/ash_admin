4e2b49536c4fd838b715ef26ab22167f429ba0d3
Peter Hartman <me@peterhartman.co.uk>
2024-03-09 09:09:56 +0000

    Allow configurable CSP nonces

--- lib/ash_admin/components/layouts.ex
+++ lib/ash_admin/components/layouts.ex
@@ -25,24 +25,32 @@ defmodule AshAdmin.Layouts do
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
         <meta name="csrf-token" content={get_csrf_token()} />
         <title><%= assigns[:page_title] || "Ash Admin" %></title>
-        <style nonce="ash_admin-Ed55GFnX">
+        <style nonce={csp_nonce(@conn, :style)}>
           <%= raw(render("app.css", %{})) %>
         </style>
         <link
+          nonce={csp_nonce(@conn, :style)}
           href="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.5.1/jsoneditor.min.css"
           rel="stylesheet"
           type="text/css"
         />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.5.1/jsoneditor.min.js">
+        <script
+          nonce={csp_nonce(@conn, :script)}
+          src="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.5.1/jsoneditor.min.js"
+        >
         </script>
-        <link rel="stylesheet" href="https://unpkg.com/easymde/dist/easymde.min.css" />
-        <script src="https://unpkg.com/easymde/dist/easymde.min.js">
+        <link
+          nonce={csp_nonce(@conn, :style)}
+          rel="stylesheet"
+          href="https://unpkg.com/easymde/dist/easymde.min.css"
+        />
+        <script nonce={csp_nonce(@conn, :script)} src="https://unpkg.com/easymde/dist/easymde.min.js">
         </script>
       </head>
       <body>
         <%= @inner_content %>
       </body>
-      <script nonce="ash_admin-Ed55GFnX">
+      <script nonce={csp_nonce(@conn, :script)}>
         <%= raw(render("app.js", %{})) %>
       </script>
     </html>
@@ -57,5 +65,15 @@ defmodule AshAdmin.Layouts do
 
   def live_socket_path(conn) do
     [Enum.map(conn.script_name, &["/" | &1]) | conn.private.live_socket_path]
+  end
+
+  defp csp_nonce(conn, type) when type in [:script, :style, :img] do
+    csp_nonce_value = conn.private.ash_admin_csp_nonce[type]
+
+    case csp_nonce_value do
+      key when is_atom(key) -> conn.assigns[csp_nonce_value]
+      key when is_bitstring(key) -> csp_nonce_value
+      _ -> raise("Unexpected type of :csp_nonce_assign_key")
+    end
   end
 end
--- test/page_live_test.exs
+++ test/page_live_test.exs
@@ -1,6 +1,7 @@
 defmodule AshAdmin.Test.PageLiveTest do
   use ExUnit.Case, async: false
 
+  import Plug.Conn
   import Phoenix.ConnTest
   import Phoenix.LiveViewTest
   @endpoint AshAdmin.Test.Endpoint
@@ -21,5 +22,41 @@ defmodule AshAdmin.Test.PageLiveTest do
     assert html =~ "Attributes"
     assert html =~ "body"
     assert html =~ "String"
+  end
+
+  test "embeds default csp nonces" do
+    html =
+      build_conn()
+      |> get("/api/admin")
+      |> html_response(200)
+
+    assert html =~ "ash_admin-Ed55GFnX"
+    assert html =~ ~s|<script nonce="ash_admin-Ed55GFnX"|
+    assert html =~ ~s|<style nonce="ash_admin-Ed55GFnX"|
+  end
+
+  test "embeds user selected csp nonces" do
+    html =
+      build_conn()
+      |> assign(:csp_nonce_value, "csp_nonce")
+      |> get("/api/csp/admin")
+      |> html_response(200)
+
+    assert html =~ ~s|<script nonce="csp_nonce"|
+    assert html =~ ~s|<style nonce="csp_nonce"|
+    assert html =~ ~s|<link nonce="csp_nonce"|
+    refute html =~ "ash_admin-Ed55GFnX"
+
+    html =
+      build_conn()
+      |> assign(:script_csp_nonce, "script_nonce")
+      |> assign(:style_csp_nonce, "style_nonce")
+      |> get("/api/csp-full/admin")
+      |> html_response(200)
+
+    assert html =~ ~s|<script nonce="script_nonce"|
+    assert html =~ ~s|<style nonce="style_nonce"|
+    assert html =~ ~s|<link nonce="style_nonce"|
+    refute html =~ "ash_admin-Ed55GFnX"
   end
 end
