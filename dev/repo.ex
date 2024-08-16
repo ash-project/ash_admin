5e3acf10e835d8a94a4540bfaeff2d9d7d7d5ece
Zach Daniel <zach@zachdaniel.dev>
2024-08-16 10:01:37 -0400

    chore: fix build

--- dev/repo.ex
+++ dev/repo.ex
@@ -1,6 +1,10 @@
 defmodule Demo.Repo do
   use AshPostgres.Repo, otp_app: :ash_admin
 
+  def min_pg_version() do
+    %Version{major: 16, minor: 0, patch: 0}
+  end
+
   def installed_extensions() do
     ["uuid-ossp", "pg_trgm", "citext", "ash-functions"]
   end
