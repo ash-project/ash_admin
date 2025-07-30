ddbdf1f6e6b4608ed3de1b5f89ec967c95e64040
Andy LeClair <andyleclair@gmail.com>
2025-07-30 13:46:43 -0400

    improvement: change svg icons to words (#351)

--- lib/ash_admin/components/top_nav/actor_select.ex
+++ lib/ash_admin/components/top_nav/actor_select.ex
@@ -22,54 +22,12 @@ defmodule AshAdmin.Components.TopNav.ActorSelect do
       <div>
         <span>
           <button phx-click={@toggle_authorizing} type="button">
-            <svg
-              :if={@authorizing}
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9.655 8H2.333c-.264 0-.398.068-.471.121a.73.73 0 0 0-.224.296 1.626 1.626 0 0 0-.138.59V14c0 .342.076.531.14.635.064.106.151.18.256.237a1.122 1.122 0 0 0 .436.127l.013.001h7.322c.264 0 .398-.068.471-.121a.73.73 0 0 0 .224-.296 1.627 1.627 0 0 0 .138-.59V9c0-.342-.076-.531-.14-.635a.658.658 0 0 0-.255-.237A1.122 1.122 0 0 0 9.655 8zm.012-1H2.333C.5 7 .5 9 .5 9v5c0 2 1.833 2 1.833 2h7.334c1.833 0 1.833-2 1.833-2V9c0-2-1.833-2-1.833-2zM8.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
-              />
-            </svg>
-            <svg
-              :if={!@authorizing}
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
-              />
-            </svg>
+            <span :if={@authorizing}>Authorizing</span>
+            <span :if={!@authorizing}>Not Authorizing</span>
           </button>
           <button :if={@actor} phx-click={@toggle_actor_paused} type="button">
-            <svg
-              :if={@actor_paused}
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-            </svg>
-            <svg
-              :if={!@actor_paused}
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
-            </svg>
+            <span :if={@actor_paused}>Actor Paused</span>
+            <span :if={!@actor_paused}>Actor Active</span>
           </button>
           <.link
             :if={@actor}
