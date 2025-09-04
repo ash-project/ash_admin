f78dee96eac6ba40113cf732a36ec72d9a90ec1c
Zach Daniel <zach@zachdaniel.dev>
2025-09-04 16:16:44 -0400

    fix: set brand on loading to_one relationships

--- lib/ash_admin/components/resource/resource.ex
+++ lib/ash_admin/components/resource/resource.ex
@@ -28,120 +28,121 @@ defmodule AshAdmin.Components.Resource do
     <div class="flex flex-col">
       <Nav.nav resource={@resource} domain={@domain} action={@action} table={@table} prefix={@prefix} />
       <div class="flex-1 p-6">
-      <div :if={@record && match?({:error, error} when not is_nil(error), @record)}>
-        <p>Error loading record</p>
-      </div>
-      <div :if={
-        @record && match?({:ok, record} when not is_nil(record), @record) &&
-          @action_type == :update
-      }>
-        <% {:ok, record} = @record %>
+        <div :if={@record && match?({:error, error} when not is_nil(error), @record)}>
+          <p>Error loading record</p>
+        </div>
+        <div :if={
+          @record && match?({:ok, record} when not is_nil(record), @record) &&
+            @action_type == :update
+        }>
+          <% {:ok, record} = @record %>
+          <.live_component
+            module={Form}
+            polymorphic_actions={@polymorphic_actions}
+            type={:update}
+            record={record}
+            resource={@resource}
+            url_path={@url_path}
+            params={@params}
+            action={@action}
+            domain={@domain}
+            id={update_id(@resource)}
+            actor={@actor}
+            authorizing={@authorizing}
+            tenant={@tenant}
+            table={@table}
+            tables={@tables}
+            prefix={@prefix}
+          />
+        </div>
+        <div :if={
+          @record && match?({:ok, record} when not is_nil(record), @record) &&
+            @action_type == :destroy
+        }>
+          <% {:ok, record} = @record %>
+          <.live_component
+            module={Form}
+            polymorphic_actions={@polymorphic_actions}
+            type={:destroy}
+            record={record}
+            resource={@resource}
+            url_path={@url_path}
+            action={@action}
+            params={@params}
+            domain={@domain}
+            id={destroy_id(@resource)}
+            actor={@actor}
+            authorizing={@authorizing}
+            tenant={@tenant}
+            table={@table}
+            tables={@tables}
+            prefix={@prefix}
+          />
+        </div>
         <.live_component
+          :if={match?({:ok, %_{}}, @record) && @action_type == :read}
+          module={Show}
+          resource={@resource}
+          domain={@domain}
+          id={show_id(@resource)}
+          record={unwrap(@record)}
+          actor={@actor}
+          authorizing={@authorizing}
+          tenant={@tenant}
+          table={@table}
+          prefix={@prefix}
+        />
+        <Info.info :if={is_nil(@action_type)} resource={@resource} domain={@domain} prefix={@prefix} />
+        <.live_component
+          :if={@action_type == :create}
           module={Form}
-          polymorphic_actions={@polymorphic_actions}
-          type={:update}
-          record={record}
+          type={:create}
           resource={@resource}
           url_path={@url_path}
           params={@params}
-          action={@action}
           domain={@domain}
-          id={update_id(@resource)}
+          action={@action}
+          id={create_id(@resource)}
           actor={@actor}
           authorizing={@authorizing}
           tenant={@tenant}
           table={@table}
           tables={@tables}
           prefix={@prefix}
-        />
-      </div>
-      <div :if={
-        @record && match?({:ok, record} when not is_nil(record), @record) && @action_type == :destroy
-      }>
-        <% {:ok, record} = @record %>
-        <.live_component
-          module={Form}
           polymorphic_actions={@polymorphic_actions}
-          type={:destroy}
-          record={record}
+        />
+        <.live_component
+          :if={@action_type == :read && !match?({:ok, %_{}}, @record)}
+          module={DataTable}
+          polymorphic_actions={@polymorphic_actions}
           resource={@resource}
-          url_path={@url_path}
           action={@action}
-          params={@params}
-          domain={@domain}
-          id={destroy_id(@resource)}
           actor={@actor}
+          domain={@domain}
+          url_path={@url_path}
+          params={@params}
+          id={data_table_id(@resource)}
           authorizing={@authorizing}
-          tenant={@tenant}
           table={@table}
           tables={@tables}
           prefix={@prefix}
+          tenant={@tenant}
         />
-      </div>
-      <.live_component
-        :if={match?({:ok, %_{}}, @record) && @action_type == :read}
-        module={Show}
-        resource={@resource}
-        domain={@domain}
-        id={show_id(@resource)}
-        record={unwrap(@record)}
-        actor={@actor}
-        authorizing={@authorizing}
-        tenant={@tenant}
-        table={@table}
-        prefix={@prefix}
-      />
-      <Info.info :if={is_nil(@action_type)} resource={@resource} domain={@domain} prefix={@prefix} />
-      <.live_component
-        :if={@action_type == :create}
-        module={Form}
-        type={:create}
-        resource={@resource}
-        url_path={@url_path}
-        params={@params}
-        domain={@domain}
-        action={@action}
-        id={create_id(@resource)}
-        actor={@actor}
-        authorizing={@authorizing}
-        tenant={@tenant}
-        table={@table}
-        tables={@tables}
-        prefix={@prefix}
-        polymorphic_actions={@polymorphic_actions}
-      />
-      <.live_component
-        :if={@action_type == :read && !match?({:ok, %_{}}, @record)}
-        module={DataTable}
-        polymorphic_actions={@polymorphic_actions}
-        resource={@resource}
-        action={@action}
-        actor={@actor}
-        domain={@domain}
-        url_path={@url_path}
-        params={@params}
-        id={data_table_id(@resource)}
-        authorizing={@authorizing}
-        table={@table}
-        tables={@tables}
-        prefix={@prefix}
-        tenant={@tenant}
-      />
-      <.live_component
-        :if={@action_type == :action}
-        module={GenericAction}
-        id={action_id(@resource)}
-        resource={@resource}
-        action={@action}
-        actor={@actor}
-        domain={@domain}
-        url_path={@url_path}
-        params={@params}
-        authorizing={@authorizing}
-        table={@table}
-        prefix={@prefix}
-        tenant={@tenant}
-      />
+        <.live_component
+          :if={@action_type == :action}
+          module={GenericAction}
+          id={action_id(@resource)}
+          resource={@resource}
+          action={@action}
+          actor={@actor}
+          domain={@domain}
+          url_path={@url_path}
+          params={@params}
+          authorizing={@authorizing}
+          table={@table}
+          prefix={@prefix}
+          tenant={@tenant}
+        />
       </div>
     </div>
     """
