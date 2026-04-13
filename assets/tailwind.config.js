c5ea2e4fceab57e70d18795e0220ee0237088d16
Zach Daniel <zach@zachdaniel.dev>
2026-04-12 20:38:53 -0400

    Revert "chore: replace vendored heroicons with git dependency (#387)"
    This reverts commit 57b9d29aea49386544a103d305f55499e0153137.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
     // See your `CoreComponents.icon/1` for more information.
     //
     plugin(function ({ matchComponents, theme }) {
-      let iconsDir = path.join(__dirname, "../deps/heroicons/optimized");
+      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized");
       let values = {};
       let icons = [
         ["", "/24/outline"],
